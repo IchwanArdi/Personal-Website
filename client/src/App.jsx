45814e279705c0343df0308a8176dabee12250d0
IchwanArdi <ichwanpwt22@gmail.com>
2025-08-15 10:42:17 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/common/Navbar';
+
+function App() {
+  return (
+    <>
+      <div className="bg-slate-950 min-h-screen">
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
